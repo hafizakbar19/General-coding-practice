71ab271e060c04071bf6b90107f4f124cf392861
Mohammed Akbar Alli <hafizakbar19@gmail.com>
2023-03-31 23:14:16 +0500

    functions

---
+++ JK-Sessions/app..js
@@ -0,0 +1,4 @@
+console.log("Hello world!");
+setTimeout(function(){
+    console.log("I will work after 2 seconds")
+},2000);
---
+++ SMIT/app.js
@@ -0,0 +1,50 @@
+
+// //finding something in the array
+// let cities = ["Karachi", "Lahore","Islamabad","Peshawar"];
+// let city = prompt("Enter city name: ");
+// let found = false;
+
+// for( let i = 0; i < cities.length; i++){
+//     if(city.toLocaleLowerCase() === cities[i].toLocaleLowerCase()){
+//         console.log("Found in the list of cities " + city);
+//         found = true;
+//         break;
+//     }
+// }
+// if(!found){
+//     console.log("Not found! " + city);
+// }
+
+//nested loops
+
+// for (let i = 1; i <=15; i++){
+//     let star = "";
+//     for(let j = 1; j <= i; j++){
+//         star = star + "* ";
+//     }console.log(star);
+// } 
+
+
+// let para ="Hello this is the dummy text for the work practice";
+// para.replace("Hello","Hi");
+// console.log(para);
+
+// num = Math.random();
+// num = num.toFixed(2);
+
+// // let num = 5.5;
+// // num = Math.floor(num);
+// console.log(num);
+
+
+
+//random number game
+// let num = Math.floor(Math.random() *10);
+// let guess = +prompt("Guess the number: ");
+// if(num === guess){
+//     alert("You Win");
+// }else if(guess +1 === num || guess -1 === num){
+//     alert("You are so close the number: " + num);
+// }else{
+//     alert("You lost! the number is: "+ num);
+// }
