84e29eecbaa90d146d954e7c63df19b9a59cd3be
Mohammed Akbar Alli <hafizakbar19@gmail.com>
2023-04-14 00:04:53 +0500

    optional parameter

--- PIAIC/index.ts
+++ PIAIC/index.ts
@@ -2,5 +2,6 @@
 // const func = (a: number, b:number) => a +b;
 
 const func (firstName:string,lastName?:string)=>{
-    return `${firstName} ${lastName}`;
-}
+    return firstName + " " + lastName;
+};
+console.log(func("Akbar", "Ali"));
