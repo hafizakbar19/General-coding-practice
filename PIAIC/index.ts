423e37f62e4ef41e0e390da1cc0c5f3c11aa5943
Mohammed Akbar Alli <hafizakbar19@gmail.com>
2023-04-13 23:59:04 +0500

    .

--- PIAIC/index.ts
+++ PIAIC/index.ts
@@ -1,2 +1,6 @@
 //function
-const func = (a: number, b:number) => a +b;
+// const func = (a: number, b:number) => a +b;
+
+const func (firstName:string,lastName?:string)=>{
+    return `${firstName} ${lastName}`;
+}
