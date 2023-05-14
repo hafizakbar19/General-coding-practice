8170ff16f252b4394f46fe87030a557ebe8288d7
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-14 18:03:25 +0500

    general practice

---
+++ BuildingsData/script.js
@@ -0,0 +1,11 @@
+ function Building(name,noApts,maint,waterSource) {
+    this.name = name,
+    this.noApts = noApts,
+    this.maint = maint,
+    this.waterSource = waterSource
+};
+
+Building.prototype.func = function(){
+
+};
+
