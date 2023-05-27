00ac5b36e4ff6b770c023801b3e9d09305db6ccc
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-27 17:04:56 +0500

    modules

--- mimo/modules/main.js
+++
@@ -1,5 +0,0 @@
-import {add} from "./library.js";
-
-console.log(add(3,5));
-
-// console.log(a);
---
+++ mimo/modules/main.mjs
@@ -0,0 +1,5 @@
+import {add, a} from "./library.mjs";
+
+// console.log(add(3,5));
+
+console.log(a);
