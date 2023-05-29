e140acceb4372a3cf446924aafc16047c7dff831
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-29 22:15:14 +0500

    .

--- mimo/reactBasic/app.js
+++ mimo/reactBasic/app.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+const header = <h1>Hello John Hello</h1>; // unlike pure HTML , we can use variables in jsx
+const using = (
+    <div>
+        {header}
+    </div>
+)
 ReactDOM.render(
-    <h1>Hello John</h1>,
+    using,
     document.getElementById("root")
 );
